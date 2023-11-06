26f9cbe284a6f2c90c399dd42e342e0132f753bb
David Rhoderick <david@vrio.com>
2023-11-07 00:31:20 +0100

    Fixing install script

--- install.sh
+++ install.sh
@@ -1,26 +1,17 @@
 # Install git
-dnf install git -y
+sudo dnf install git -y
 
 # Clone repo
-git clone git@github.com:toptal-dave/ec2-bash-fastify.git ./
+git clone https://github.com/toptal-dave/ec2-bash-fastify.git ./app
 
 # Install n node version manager and installation of the LTS version
+sudo dnf install make -y
 curl -L https://bit.ly/n-install | bash -s -- -y
+. /home/ec2-user/.bashrc
 
 # Install project dependencies
+cd app
 npm i
 
 # Run server
-node server
-
-# # Install nginx
-# dnf install -y nginx
-
-# # Set up default listener
-# mv default /etc/nginx/sites-available/default
-
-# # Start NGINX
-# systemctl start nginx.service
-
-# # Persist NGINX on start-up
-# systemctl enable nginx.service
+npm start
