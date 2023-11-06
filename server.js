46815db36130514abe26089be7710864d16dda17
David Rhoderick <david@vrio.com>
2023-11-06 23:17:20 +0100

    Changing port back to 3000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ fastify.get("/", async function handler(request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 80 });
+  await fastify.listen({ port: 3000 });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
