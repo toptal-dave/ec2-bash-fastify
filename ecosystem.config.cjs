e03936dac94237c7241fd8d72124103ab0d7cb94
David Rhoderick <david@iskratech.rs>
2023-11-13 09:02:38 +0100

    Setting up environment port selection.

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,19 +1,25 @@
 module.exports = {
-  apps : [{
-    script: 'server.js',
-    watch: '.'
-  }],
+  apps: [
+    {
+      script: "server.js",
+      watch: ".",
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+  deploy: {
+    production: {
+      user: "SSH_USERNAME",
+      host: "SSH_HOSTMACHINE",
+      ref: "origin/master",
+      repo: "GIT_REPOSITORY",
+      path: "DESTINATION_PATH",
+      "pre-deploy-local": "",
+      "post-deploy":
+        "npm install && pm2 reload ecosystem.config.js --env production",
+      "pre-setup": "",
+    },
+  },
 };
--- server.js
+++ server.js
@@ -11,7 +11,9 @@ fastify.get("/", async function handler(request, reply) {
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({
+    port: process.env.NODE_ENV === "production" ? 80 : 3000,
+  });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
