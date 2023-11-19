dfcd2e2895e1d38053fda3363a336e9cf0d02cc6
David Rhoderick <david@vrio.com>
2023-11-19 13:27:09 +0100

    Making sure th ec2-user owns the downloaded app.

--- user-data.sh
+++ user-data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo dnf install git -y
 git clone https://github.com/toptal-dave/ec2-bash-fastify.git /home/ec2-user/app
+sudo chown -R ec2-user:ec2-user /home/ec2-user/app
 sudo -u ec2-user /home/ec2-user/app/install.sh
