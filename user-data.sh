d92207ea17842fb36e327b9a98d687c184c3176a
David Rhoderick <david@vrio.com>
2023-11-19 13:35:26 +0100

    Updating user-data to make install.sh executable

--- user-data.sh
+++ user-data.sh
@@ -2,4 +2,5 @@
 sudo dnf install git -y
 git clone https://github.com/toptal-dave/ec2-bash-fastify.git /home/ec2-user/app
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app
+sudo chmod +x /home/ec2-user/app/install.sh
 sudo -u ec2-user /home/ec2-user/app/install.sh
