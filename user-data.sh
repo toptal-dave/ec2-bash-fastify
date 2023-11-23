a882ab991f5a1116c0037fd13840e05ea20583f5
David Rhoderick <david@vrio.com>
2023-11-23 23:08:12 +0100

    Re-ordering commands to move fastify nginx conf after we install nginx.

--- user-data.sh
+++ user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 sudo dnf install git -y
 git clone https://github.com/toptal-dave/ec2-bash-fastify.git /home/ec2-user/app
-sudo mv /home/ec2-user/app/fastify.conf /etc/nginx/conf.d/fastify.conf
 sudo dnf install -y nginx
+sudo mv /home/ec2-user/app/fastify.conf /etc/nginx/conf.d/fastify.conf
 sudo systemctl start nginx.service
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app
 sudo chmod +x /home/ec2-user/app/install.sh
