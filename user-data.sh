2868272aee7ae13bc5f46f015eef234ec5d3885b
David Rhoderick <david@vrio.com>
2023-11-23 23:19:39 +0100

    Updating nginx confifugration & user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -2,7 +2,7 @@
 sudo dnf install git -y
 git clone https://github.com/toptal-dave/ec2-bash-fastify.git /home/ec2-user/app
 sudo dnf install -y nginx
-sudo mv /home/ec2-user/app/fastify.conf /etc/nginx/conf.d/fastify.conf
+sudo mv /home/ec2-user/app/nginx.conf /etc/nginx/nginx.conf
 sudo systemctl start nginx.service
 sudo chown -R ec2-user:ec2-user /home/ec2-user/app
 sudo chmod +x /home/ec2-user/app/install.sh
